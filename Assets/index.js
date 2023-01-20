aeae2e5d0e6a5d130025151e76363a720da52612
Fero Ezar Felix <ferofelix123@gmail.com>
2023-01-20 11:28:02 +0700

    new

--- Assets/index.js
+++ Assets/index.js
@@ -36,13 +36,18 @@ function clear(){
     password = document.getElementById("pass").value= "";
 }
 
-// SiGNUP--------
+                // --------SiGNUP--------
 
 document.getElementById("kmenu").onkeyup = function(){
     let txtnama = document.getElementById("kmenu").value;
-    let txtnama2 = txtnama.toUpperCase();
+    let txtnama2 = txtnama.toLowerCase();
     document.getElementById("kmenu").value = txtnama2;
 };
+document.getElementById('tpt').onkeyup = function (){
+    let txtnama = document.getElementById('tpt').value;
+    let txtnama2 = txtnama.toUpperCase();
+    document.getElementById('tpt').value = txtnama2;
+}
 
 document.getElementById("global1").onload = function(){
     let today = new Date();
@@ -54,21 +59,39 @@ document.getElementById("global1").onload = function(){
     tgl = document.getElementById('kode').value = "INV" + today + "001";
     pembayaran();
 };
-
-document.getElementById("btn").onclick = function(){
+document.getElementById("hilang").onclick = function(){
+    document.getElementById("tampil").style = "display:none;";
+}
+document.getElementById("masuk").onclick = function(){
+    document.getElementById("tampil").style = "display:inline;";
+    
     let menu = document.getElementById('kmenu').value;
+        if(menu == "" || menu == null){
+            document.getElementById("x1").style = "display:inline;";
+        }else{
+            document.getElementById('data1').innerHTML = menu;
+        }
 
-    let nama = document.getElementById('nm').value;
+    let tempat = document.getElementById('tpt').value;
+    let tanggal = document.getElementById('dat').value;
+
+        if(tempat == "" || tempat == null || tanggal == "" || tanggal == null){
+            document.getElementById("x2").style = "display:inline;";
+        }else{
+            document.getElementById("x2").style = "display:none;";
+            document.getElementById('data2').innerHTML = tempat+","+tanggal;
+        }
+    
 
     combobox();
     
     let jb = document.getElementById('jb').value;
 
     let pembayaran = "";
-        if(document.getElementById("tunai").checked == true){
-            pembayaran = "Tunai"}
-        if(document.getElementById("nonTunai").checked == true){
-            pembayaran = "Non-Tunai"}
+        if(document.getElementById("Paypal").checked == true){
+            pembayaran = "Paypal"}
+        if(document.getElementById("VISA").checked == true){
+            pembayaran = "VISA"}
         if(pembayaran == ""){
             alert("wajib dipilih")
         }
@@ -77,7 +100,7 @@ document.getElementById("btn").onclick = function(){
     total(jb,harga); 
             
     document.getElementById('data1').innerHTML = menu;
-    document.getElementById('data2').innerHTML = nama;
+    document.getElementById('data2').innerHTML = tempat+","+tanggal;
     document.getElementById('data4').innerHTML = jb
     document.getElementById('data7').innerHTML = pembayaran;
     document.getElementById("harga").value = harga
@@ -85,14 +108,17 @@ document.getElementById("btn").onclick = function(){
 
 function combobox(){
     let jenis = document.getElementById('form1').jm.value;
-        if(jenis ==  "Makanan"){
-            document.getElementById('data3').innerHTML="Makanan";
+        if(jenis ==  "Europe"){
+            document.getElementById('data3').innerHTML="Europe";
         }
-        if(jenis == "Minuman"){
-            document.getElementById('data3').innerHTML="Minuman";
+        if(jenis == "Asia"){
+            document.getElementById('data3').innerHTML="Asia";
         }
-        else if(jenis == "Snack"){
-            document.getElementById('data3').innerHTML="Snack";
+        else if(jenis == "Amerika"){
+            document.getElementById('data3').innerHTML="Amerika";
+        }
+        else if(jenis == "Australia"){
+            document.getElementById('data3').innerHTML="Australia";
         }
 }
 
@@ -114,34 +140,11 @@ function total(jb,harga){
     }
 }
 
-document.getElementById('nm').onchange = function(){
-    let bs = document.getElementById('nm').value;
-    let harga = document.getElementById('harga').value;
-    if(bs == "bakso"){
-        harga = 20000;
-    }
-    else if(bs == "mieayam"){
-        harga = 30000;
-    }
-    else{
-        harga = 10000;
-    }
-    document.getElementById("harga").value = harga;
-}
-
-document.getElementById("jb").onchange = function(){
-    let harga = document.getElementById("harga").value;
-    let jb = document.getElementById('jb').value;
-
-    let total = harga * jb
-    alert("Total harga = "+ total);
-}
-
 function pembayaran(){
-    let carabayar = ["tunai","nonTunai"];
+    let carabayar = ["Paypal","VISA"];
     let jenis = "";
     for (let i = 0; i <carabayar.length; i++){
-        jenis += '<input class="form-check-input" type="radio" name="flexRadioDefault" id="'+carabayar[i]+'" value="'+carabayar[i]+'" checked> <label class="form-check-label" for="flexRadioDefault2">'+carabayar[i]+'</label>'
+        jenis += '<input class="form-check-input" type="radio" name="flexRadioDefault" id="'+carabayar[i]+'" value="'+carabayar[i]+'" checked> <label class="form-check-label" for="flexRadioDefault2">'+carabayar[i]+'</label> <br>'
     }
         document.getElementById('jenisbayar').innerHTML = jenis;
 }
